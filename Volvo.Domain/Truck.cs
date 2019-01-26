a405e162d9b57e5c7cf5b9c45ee02de021e3a272
Diogo Pereira <diogoalexp@outlook.com>
2019-01-26 17:08:47 -0200

    add unit test

---
+++ Volvo.Domain/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Volvo.Domain
+{
+    public class Truck
+    {
+        private int id;
+        private string name;
+        private decimal value;
+        private DateTime date;
+        //private Model model;
+        private int model_id;
+
+        public Truck() { }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public decimal Value { get => value; set => this.value = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public int Model_id { get => model_id; set => model_id = value; }
+
+        //public Model Model { get => model; set => model = value; }
+    }
+}
---
+++ Volvo.Test/TruckTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Volvo.Test
+{
+    [TestClass]
+    public class TruckTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
